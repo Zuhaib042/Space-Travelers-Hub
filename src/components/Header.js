4ffca23d28f928cc6550b1789718106704a02421
Zuhaib Amjad <umar0423@gmail.com>
2022-11-15 12:57:08 +0500

    Style navbar as per requirements

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 import planet from '../images/planet.png';
 
 const Header = () => (
-  <div className="flex justify-between items-center ">
-    <div className="flex items-center">
-      <img src={planet} alt="planetImage" width="60px" />
-      <h3>Space Travelers Hub</h3>
+  <div className="flex justify-between items-center py-3 border-zinc-300 border-b">
+    <div className="flex items-center ml-10">
+      <img src={planet} alt="planetImage" width="50px" />
+      <h3 className="text-3xl pl-2">Space Travelers&apos; Hub</h3>
     </div>
-    <ul className="flex">
-      <li>
+    <ul className="flex mr-14">
+      <li className="px-4 text-[#017bfe]">
         <NavLink
           to="/"
           style={(isActive) => ({
@@ -19,7 +19,7 @@ const Header = () => (
           Rockets
         </NavLink>
       </li>
-      <li>
+      <li className="px-4 text-[#017bfe]">
         <NavLink
           to="/missionspage"
           style={(isActive) => ({
@@ -29,7 +29,7 @@ const Header = () => (
           Missions
         </NavLink>
       </li>
-      <li>
+      <li className="px-4 border-l border-slate-500  text-[#017bfe]">
         <NavLink
           to="/myprofilepage"
           style={(isActive) => ({
