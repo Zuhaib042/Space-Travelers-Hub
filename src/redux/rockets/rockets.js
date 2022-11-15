cda86032ca853d72ae1e03611373e1b8008f2395
Zuhaib Amjad <umar0423@gmail.com>
2022-11-15 16:55:55 +0500

    Fetch data from api using ducks pattern
    Used the Axios to fetch data from api as it is more robust and less abstract method

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,56 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// Actions
+const FETCH_ROCKETS = 'FETCH_ROCKETS';
+
+// state data
+const initialState = [];
+
+const baseUrl = 'https://api.spacexdata.com/v3/rockets';
+
+// async function fetch() {
+//   try {
+//     const response = await axios.get(baseUrl);
+//     const rockets = response.data;
+//     const res = Object.keys(rockets).map((id) => ({
+//       id: rockets[id].id,
+//       title: rockets[id].rocket_name,
+//       image: rockets[id].flickr_images[0],
+//       decription: rockets[id].description,
+//     }));
+//     console.log(res);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// fetch();
+
+// Reducers
+
+const rocketReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'FETCH_ROCKETS/fulfilled': {
+      return [...state, action.payload];
+    }
+    default:
+      return state;
+  }
+};
+
+// action creators
+
+export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async () => {
+  const response = await axios.get(baseUrl);
+  const rockets = response.data;
+  const res = Object.keys(rockets).map((id) => ({
+    id: rockets[id].id,
+    title: rockets[id].rocket_name,
+    image: rockets[id].flickr_images[0],
+    decription: rockets[id].description,
+  }));
+  console.log(res);
+  return res;
+});
+
+export default rocketReducer;
