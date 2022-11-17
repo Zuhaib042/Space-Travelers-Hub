35a5f2d0cf67dcfcf268b8192883445001852a19
Zuhaib Amjad <umar0423@gmail.com>
2022-11-17 15:05:28 +0500

    Refactor profilePage on DRY principle

--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -1,72 +1,48 @@
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-
-// const MyProfilePage = () => {
-//   const allRockets = useSelector((state) => state.rocket);
-//   const rocketProfile = allRockets.filter((rocket) => rocket.reserved);
-
-//   return (
-//     <div className="mt-4 grid grid-cols-1 md:grid-cols-2 md:gap-x-2 gap-y-2 mx-auto w-[75%]">
-//       <div className="">
-//         <h2 className="text-xl">Missions</h2>
-//         <ul className=" mt-3">
-//           <li className="border">Mars</li>
-//         </ul>
-//       </div>
-//       <div className="">
-//         <h2 className="text-xl">Rockets</h2>
-//         {rocketProfile.length > 0 ? (
-//           <ul className="mt-3 border divide-y-[1px]">
-//             {rocketProfile.map((rocket) => (
-//               <li key={rocket.id} className="py-3 pl-2">
-//                 {rocket.title}
-//               </li>
-//             ))}
-//           </ul>
-//         ) : (
-//           ''
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MyProfilePage;
-
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectAllmissions, unjoinMission } from '../redux/missions/MissionsSlice';
+import { useSelector } from 'react-redux';
+import { selectAllmissions } from '../redux/missions/MissionsSlice';
 
-function MyProfilePage() {
-  const dispatch = useDispatch();
+const MyProfilePage = () => {
+  // rockets reservation
+  const allRockets = useSelector((state) => state.rocket);
+  const rocketProfile = allRockets.filter((rocket) => rocket.reserved);
+
+  // missions reservation
   const reservation = useSelector(selectAllmissions);
-  const missiondata = reservation.filter((item) => item.missionjoin === true);
-
-  const cancelmissionHandler = (event) => {
-    const missioncancel = event.target.id;
-    dispatch(unjoinMission(missioncancel));
-  };
+  const missiondata = reservation.filter((item) => item.missionjoin);
 
   return (
-    <section className="table-container">
-      <div className="missions">
-        <span className="text-2xl">My Missions</span>
-        {!missiondata.length ? (<span>There are no missions joined.</span>) : null }
-        <div className="items-list">
-          {missiondata && missiondata.map((elem) => (
-            <div className="list-box" key={elem.missionid}>
-              <span className="list-name">
-                {elem.missionName}
-              </span>
-              <button className="remBtn" type="button" id={elem.missionid} onClick={cancelmissionHandler}>
-                Cancel
-              </button>
-            </div>
-          ))}
-        </div>
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 md:gap-x-8 gap-y-6 mx-auto ml-8 mr-12">
+      <div className="">
+        <h2 className="text-xl">Missions</h2>
+        {missiondata.length > 0 ? (
+          <ul className="mt-3 border divide-y-[1px]">
+            {missiondata.map((mission) => (
+              <li key={mission.missionid} className="py-3 pl-2">
+                {mission.missionName}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          ''
+        )}
       </div>
-    </section>
+      <div className="">
+        <h2 className="text-xl">Rockets</h2>
+        {rocketProfile.length > 0 ? (
+          <ul className="mt-3 border divide-y-[1px]">
+            {rocketProfile.map((rocket) => (
+              <li key={rocket.id} className="py-3 pl-2">
+                {rocket.title}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default MyProfilePage;
